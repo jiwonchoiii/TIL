982abf64f783fdd986dd11fe4ce26d9170165006
jiwonchoiii <82692567+jiwonchoiii@users.noreply.github.com>
2021-07-11 23:32:07 +0900

    Update Main.java

--- Java/CODE/기타/bicycle/Main.java
+++ Java/CODE/기타/bicycle/Main.java
@@ -3,7 +3,7 @@ package bicycle;
 public class Main {
 	public static void main(String[] args) {
 
-		Bicycle bicycle = new Bicycle("자전거회사1", "BICY1001-123", "RED", 10, 150000);
+		Bicycle bicycle = new Bicycle("자전거회사1", "BICY1001-123", "RED", 10, 200000);
 		
 		bicycle.run();
 		
